5a76ee34b8cd4982995b121cde02c1ca4c700f56
Jarno Rantanen <jarno@jrw.fi>
2016-01-23 13:20:09 +0200

    Add basic HDMI output control scripts.

---
+++ dist/display-off.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo tvservice -o > /dev/null
---
+++ dist/display-on.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo tvservice -p > /dev/null && sudo chvt 1 && sudo chvt 8 # cycling virtual terminals helps wake up the display
