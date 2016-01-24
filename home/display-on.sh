b238700c1732d86246b888696fe5b0c61fb19322
Jarno Rantanen <jarno@jrw.fi>
2016-01-24 20:32:40 +0200

    Update comment about chvt in the display-on script.

--- home/display-on.sh
+++ home/display-on.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo tvservice -p > /dev/null && sudo chvt 1 && sudo chvt 8 # cycling virtual terminals helps wake up the display
+sudo tvservice -p > /dev/null && sudo chvt 1 && sudo chvt 8 # for whatever reason, cycling virtual terminals helps wake up the display in some cases
