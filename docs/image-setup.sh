4fe960a6668339edd1a0fd983265515dabce723b
Jarno Rantanen <jarno@jrw.fi>
2020-03-10 12:24:35 +0200

    Separate the SSH disabling and eventual poweroff steps in image-setup.

--- docs/image-setup.sh
+++ docs/image-setup.sh
@@ -250,8 +250,11 @@ rm temp
 working "Removing SSH host keys & enable regeneration"
 ssh "sudo rm -f -v /etc/ssh/ssh_host_*_key* && sudo systemctl enable regenerate_ssh_host_keys"
 
-working "Removing temporary SSH pubkey, disabling SSH & shutting down"
-ssh "(echo > .ssh/authorized_keys) && sudo systemctl disable ssh && sudo shutdown -h now"
+working "Removing temporary SSH pubkey & disabling SSH"
+ssh "(echo > .ssh/authorized_keys) && sudo systemctl disable ssh"
+
+working "Powering off the Pi"
+ssh "sudo poweroff"
 
 question "Eject the SD card from the Pi, and mount it back to this computer (press enter when ready)"
 read
