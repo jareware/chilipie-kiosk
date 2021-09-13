5bf2b26fbee9b07eba819928b9cb08c1629a42b3
Jiří Houžvička <houzvickajiri@gmail.com>
2021-09-13 20:05:11 +0200

    Update display-on.sh

--- home/display-on.sh
+++ home/display-on.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo tvservice -p > /dev/null && sudo chvt 2 && sudo chvt 1 # for whatever reason, cycling virtual terminals helps wake up the display in some cases
+sudo vcgencmd display_power 1 > /dev/null
